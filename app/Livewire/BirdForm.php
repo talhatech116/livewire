bd6402842953454e66ac67ef3ac7ac42811c8a02
Talha Tech <talhatech116@gmail.com>
2025-07-05 22:48:42 +0500

    Form validation

--- app/Livewire/BirdForm.php
+++ app/Livewire/BirdForm.php
@@ -7,8 +7,10 @@ use App\Models\Entry;
 
 class BirdForm extends Component
 {   
-
+    #[Validate("required|integer")]
     public int $bird_count;
+
+    #[Validate("required|string")]
     public string $notes;
 
     public function submit(){
@@ -22,6 +24,8 @@ class BirdForm extends Component
 
     public function render()
     {
-        return view('livewire.bird-form');
+        return view('livewire.bird-form', [
+            'enteries' => Entry::all(),
+        ]);
     }
 }
--- resources/views/livewire/bird-form.blade.php
+++ resources/views/livewire/bird-form.blade.php
@@ -1,17 +1,27 @@
 <div>
-
-    <form wire:submit="submit">
+    <form wire:submit.prevent="submit">
         <div>
             <label for="count">Bird Count</label>
-            <input wire:model='bird_count' type="number"/>
+            <input wire:model='bird_count' type="number" />
         </div>
         
         <div>
             <label for="notes">Notes</label>
-            <textarea wire:model='notes' ></textarea>
+            <textarea wire:model='notes'></textarea>
         </div>
       
-        <Button>Add new Bird Entry</Button>
+        <button type="submit">Add new Bird Entry</button>
     </form>
-  
+
+    <div>
+        @foreach ($enteries as $entry)
+            <div>
+                <strong>Count:</strong> {{ $entry->bird_count }}
+            </div>
+            <div>
+                <strong>Notes:</strong> {{ $entry->notes }}
+            </div>
+            <hr>
+        @endforeach
+    </div>
 </div>
